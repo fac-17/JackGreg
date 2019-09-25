817dc195349b4cc8ce6be8d83be1a072b87cc0cc
Gregor <gregorlthomson@sky.com>
2019-09-25 15:23:19 +0100

    Establish header skeleton

--- src/Header.js
+++ src/Header.js
@@ -1,3 +1,13 @@
 import React from "react";
 
-const Header = () => {};
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="#" alt="Home logo" />
+      <p>Home</p>
+      <h2>Hula Heaven</h2>
+      <img src="#" alt="Restart logo" />
+      <p>Restart</p>
+    </header>
+  );
+};
