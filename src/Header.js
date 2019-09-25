552a8f66cfb4e5fe8186277a6415f2ab451648b7
Gregor <gregorlthomson@sky.com>
2019-09-25 15:31:57 +0100

    Add images for header

--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,10 @@ import React from "react";
 const Header = () => {
   return (
     <header className="header">
-      <img src="#" alt="Home logo" />
+      <img src="./circus.svg" alt="Home logo" />
       <p>Home</p>
       <h2>Hula Heaven</h2>
-      <img src="#" alt="Restart logo" />
+      <img src="./restart.svg" alt="Restart logo" />
       <p>Restart</p>
     </header>
   );
