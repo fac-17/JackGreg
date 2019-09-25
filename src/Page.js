b1a740ce5a98c72279a0dd83c4d47033abc057e8
Jack Bridger <jacksbridger@gmail.com>
2019-09-25 15:57:17 +0100

    add a button that goes to the final page with the score

--- src/Page.js
+++ src/Page.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Hula from './Hula';
 
 function Page() {
-    const [currentPage, setCurrentPage] = React.useState('form');
+    const [currentPage, setCurrentPage] = React.useState('hula');
+    const [score, setScore] = React.useState(0);
     if (currentPage === 'form') {
         return (<div>I will be a Form</div>);
     }
     else if (currentPage === 'hula') {
-        return (< Hula />);
+        return (< Hula setScore={setScore} score={score} setCurrentPage={setCurrentPage} />);
     }
     else if (currentPage === 'score') {
         return (<div>I will be the score summary</div>);
