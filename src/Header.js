eaa41a20258b21c959576384ee9a608f1385bf30
Jack Bridger <jacksbridger@gmail.com>
2019-09-26 12:21:28 +0100

    basic footer styling

--- src/Header.js
+++ src/Header.js
@@ -6,12 +6,12 @@ const Header = () => {
     <header className="header">
       <section className="navbar">
         <section className="navbar--home">
-          <img src="./circus.svg" alt="Home logo" />
+          <img className="icons" src="./circus.svg" alt="Home logo" />
           <p>Home</p>
         </section>
         <h2>Hula Heaven</h2>
         <section className="navbar--restart">
-          <img src="./restart.svg" alt="Restart logo" />
+          <img className="icons" src="./restart.svg" alt="Restart logo" />
           <p>Restart</p>
         </section>
       </section>
