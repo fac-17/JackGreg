7bb357e76d7f0d5a6094d7bd8a5a5ff05148f267
Jack Bridger <jacksbridger@gmail.com>
2019-09-26 12:10:57 +0100

    implement basic navbar

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,21 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
     <header className="header">
-      <img src="./circus.svg" alt="Home logo" />
-      <p>Home</p>
-      <h2>Hula Heaven</h2>
-      <img src="./restart.svg" alt="Restart logo" />
-      <p>Restart</p>
+      <section className="navbar">
+        <section className="navbar--home">
+          <img src="./circus.svg" alt="Home logo" />
+          <p>Home</p>
+        </section>
+        <h2>Hula Heaven</h2>
+        <section className="navbar--restart">
+          <img src="./restart.svg" alt="Restart logo" />
+          <p>Restart</p>
+        </section>
+      </section>
+      <img className="elipses" src="./Elipses.png" alt="elipses" />
     </header>
   );
 };
