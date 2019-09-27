43fc3e2a2598e8e976f97b396ab9ce473c38480d
Gregor <gregorlthomson@sky.com>
2019-09-27 11:39:08 +0100

    Refactor Header

--- src/Header.js
+++ src/Header.js
@@ -5,17 +5,8 @@ const Header = () => {
   return (
     <header className="header">
       <section className="navbar">
-        <section className="navbar--home">
-          <img className="icons" src="./circus.svg" alt="Home logo" />
-          <p>Home</p>
-        </section>
         <h2>Hula Heaven</h2>
-        <section className="navbar--restart">
-          <img className="icons" src="./restart.svg" alt="Restart logo" />
-          <p>Restart</p>
-        </section>
       </section>
-      {/* <img className="elipses" src="./elipses.png" alt="elipses" /> */}
     </header>
   );
 };
